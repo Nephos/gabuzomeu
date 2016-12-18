bf2820ca1bf0770ced329ac1d03a2ec909149bb4
Arthur Poulet <arthur.poulet@mailoo.org>
2016-12-19 00:54:27 +0100

    Fix: handle bignum

--- lib/gabuzomeu.rb
+++ lib/gabuzomeu.rb
@@ -1,5 +1,5 @@
 module Gabuzomeu
-  module Fixnum
+  module Integer
 
     GABUZOMEU = %w(ga bu zo meu)
 
@@ -13,6 +13,14 @@ module Gabuzomeu
   end
 end
 
+class Integer
+  prepend Gabuzomeu::Integer
+end
+
 class Fixnum
-  prepend Gabuzomeu::Fixnum
+  prepend Gabuzomeu::Integer
+end
+
+class Bignum
+  prepend Gabuzomeu::Integer
 end
